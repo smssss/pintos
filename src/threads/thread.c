58143dffb26c8f8e225f807c7a3417318518e640
Xiaoyun <zxydut@gmail.com>
2016-01-24 21:22:13 -0700

    v3 --pass priority-change

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -70,6 +70,7 @@ static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
+static bool priority_comp (const struct list_elem *a, const struct list_elem *b, void *aux);
 
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
@@ -201,6 +202,11 @@ thread_create (const char *name, int priority,
   /* Add to run queue. */
   thread_unblock (t);
 
+  /* Yield if the new thread has a higher priority */
+  struct thread *front_t = list_entry (list_front (&ready_list), struct thread, elem);
+  if (thread_current()->priority < front_t->priority) 
+	thread_yield();
+   
   return tid;
 }
 
@@ -237,9 +243,19 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+
+  list_insert_ordered(&ready_list, &t->elem, priority_comp, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
+}
+
+static bool
+priority_comp (const struct list_elem *a, const struct list_elem *b, void *aux)
+{
+  ASSERT(a != NULL && b != NULL);
+  struct thread *t1 = list_entry (a, struct thread, elem);
+  struct thread *t2 = list_entry (b, struct thread, elem);
+  return t1->priority > t2->priority;
 }
 
 /* Returns the name of the running thread. */
@@ -308,7 +324,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered(&ready_list, &cur->elem, priority_comp, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -335,7 +351,10 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  struct thread *front_t = list_entry (list_front (&ready_list), struct thread, elem);
+  thread_current()->priority = new_priority;
+  if (thread_current()->priority < front_t->priority) 
+	thread_yield();
 }
 
 /* Returns the current thread's priority. */
